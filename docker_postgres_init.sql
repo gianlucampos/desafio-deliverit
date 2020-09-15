aa2c6330b0aa7dc443acec1388745f344820916e
gianlucampos <gianlucampos97@gmail.com>
2020-09-14 23:49:59 -0300

    Maven funcionando no windows, falta testar no linux

--- docker_postgres_init.sql
+++ docker_postgres_init.sql
@@ -1,9 +1,9 @@
 -- CREATE USER gianluca WITH PASSWORD 'teste12' CREATEDB;
+
 CREATE DATABASE crudGenerico
-    WITH 
-    OWNER = postgres
+    WITH OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'pt_br.utf8'
-    LC_CTYPE = 'pt_br.utf8'
+    LC_COLLATE = 'pt_BR.utf8'
+    LC_CTYPE = 'pt_BR.utf8'
     TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
+    CONNECTION LIMIT = -1;"
